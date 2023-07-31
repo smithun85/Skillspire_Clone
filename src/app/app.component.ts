8b04a5b80567202ab09307fe2d11773b3d9da070
smithun85 <nouman.puthawala@iconflux.com>
2023-07-31 10:51:21 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Hotel_Booking_App';
+}
